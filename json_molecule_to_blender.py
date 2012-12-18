7f84a79d1c2643b5a9ec736dda1a34a18330b8c7
Patrick Fuller <patrickfuller@gmail.com>
2012-12-18 17:23:47 -0600

    ran through autopep8

--- json_molecule_to_blender.py
+++ json_molecule_to_blender.py
@@ -2,7 +2,7 @@
 """
 Loads a json molecule and draws atoms in Blender.
 
-Blender scripts are weird. Either run this inside of Blender or in a shell with:
+Blender scripts are weird. Either run this inside of Blender or in a shell with
     blender foo.blend -P molecule_to_blender.py
 
 The script expects an input file named "molecule.json" and should be in the
@@ -35,7 +35,8 @@ for key in available_atoms:
     bpy.data.materials[key].diffuse_color = colors[key]
     bpy.data.materials[key].specular_intensity = 0.2
 
-def draw_molecule(molecule, center=(0,0,0), max_molecule_size=5, 
+
+def draw_molecule(molecule, center=(0, 0, 0), max_molecule_size=5,
                   show_bonds=True):
     """ Draw a molecule to blender. Uses loaded json molecule data. """
 
@@ -47,8 +48,9 @@ def draw_molecule(molecule, center=(0,0,0), max_molecule_size=5,
 
     # Scale location coordinates and add specified center
     for atom in molecule["atoms"]:
-        atom["location"] = [c+x*scale for c,x in zip(center, atom["location"])]
-    
+        atom["location"] = [c + x * scale for c, x in zip(center,
+                                                          atom["location"])]
+
     # Add some mesh primitives
     bpy.ops.object.select_all(action='DESELECT')
     bpy.ops.mesh.primitive_uv_sphere_add()
@@ -56,10 +58,10 @@ def draw_molecule(molecule, center=(0,0,0), max_molecule_size=5,
     bpy.ops.mesh.primitive_cylinder_add()
     cylinder = bpy.context.object
     cylinder.active_material = bpy.data.materials["bond"]
-    
+
     # Keep references to all atoms and bonds
     shapes = []
-    
+
     # If using space_filling model, scale up atom size and remove bonds
     if not show_bonds:
         scale *= 2.5
@@ -76,74 +78,76 @@ def draw_molecule(molecule, center=(0,0,0), max_molecule_size=5,
         atom_sphere = sphere.copy()
         atom_sphere.data = sphere.data.copy()
         atom_sphere.location = atom["location"]
-        atom_sphere.dimensions = [diameters[atom["element"]] * scale]*3
+        atom_sphere.dimensions = [diameters[atom["element"]] * scale] * 3
         atom_sphere.active_material = bpy.data.materials[atom["element"]]
         bpy.context.scene.objects.link(atom_sphere)
         shapes.append(atom_sphere)
 
     # Draw bonds
     for bond in molecule["bonds"]:
-    
+
         # Extracting locations
         first_loc = molecule["atoms"][bond["source"]]["location"]
         second_loc = molecule["atoms"][bond["target"]]["location"]
-        diff = [c2-c1 for c2, c1 in zip(first_loc, second_loc)]
-        cent = [(c2+c1)/2 for c2, c1 in zip(first_loc, second_loc)]
-        mag = sum([(c2-c1)**2 for c1, c2 in zip(first_loc, second_loc)])**0.5
+        diff = [c2 - c1 for c2, c1 in zip(first_loc, second_loc)]
+        cent = [(c2 + c1) / 2 for c2, c1 in zip(first_loc, second_loc)]
+        mag = sum(
+            [(c2 - c1) ** 2 for c1, c2 in zip(first_loc, second_loc)]) ** 0.5
 
         # Euler rotation calculation
-        v_axis = Vector(diff).normalized() 
-        v_obj = Vector((0,0,1))
+        v_axis = Vector(diff).normalized()
+        v_obj = Vector((0, 0, 1))
         v_rot = v_obj.cross(v_axis)
         angle = acos(v_obj.dot(v_axis))
 
         # Check that the number of bonds is logical
-        if bond["order"] not in range(1,4):
+        if bond["order"] not in range(1, 4):
             print("Improper number of bonds! Defaulting to 1.")
             bond["order"] = 1
 
         # Specify locations of each bond in every scenario
         if bond["order"] == 1:
-            trans = [[0]*3]
+            trans = [[0] * 3]
         elif bond["order"] == 2:
-            trans = [[0.7 * diameters["bond"] * x for x in v_obj], 
+            trans = [[0.7 * diameters["bond"] * x for x in v_obj],
                     [-0.7 * diameters["bond"] * x for x in v_obj]]
         elif bond["order"] == 3:
-            trans = [[0]*3, [1.1*diameters["bond"] * x for x in v_obj], 
-                    [-1.1*diameters["bond"] * x for x in v_obj]]
+            trans = [[0] * 3, [1.1 * diameters["bond"] * x for x in v_obj],
+                    [-1.1 * diameters["bond"] * x for x in v_obj]]
         # Draw bonds
         for i in range(bond["order"]):
             bond_cylinder = cylinder.copy()
             bond_cylinder.data = cylinder.data.copy()
-            bond_cylinder.dimensions = [diameters["bond"] * scale]*2 + [mag]
-            bond_cylinder.location = [c+scale*v for c,v in zip(cent, trans[i])]
+            bond_cylinder.dimensions = [diameters["bond"] * scale] * 2 + [mag]
+            bond_cylinder.location = [c + scale * v for c,
+                                      v in zip(cent, trans[i])]
             bond_cylinder.rotation_mode = "AXIS_ANGLE"
             bond_cylinder.rotation_axis_angle = [angle] + list(v_rot)
             bpy.context.scene.objects.link(bond_cylinder)
             shapes.append(bond_cylinder)
-    
+
     # Remove primitive meshes
-    bpy.ops.object.select_all(action='DESELECT') 
-    sphere.select = True 
+    bpy.ops.object.select_all(action='DESELECT')
+    sphere.select = True
     cylinder.select = True
     # If the starting cube is there, remove it
     if "Cube" in bpy.data.objects.keys():
         bpy.data.objects.get("Cube").select = True
     bpy.ops.object.delete()
-    
+
     # Smooth and join molecule shapes
     for shape in shapes:
         shape.select = True
     bpy.context.scene.objects.active = shapes[0]
     bpy.ops.object.shade_smooth()
     bpy.ops.object.join()
-    
+
     # Center object origin to geometry
     bpy.ops.object.origin_set(type="ORIGIN_GEOMETRY", center="MEDIAN")
-    
+
     # Refresh scene
     bpy.context.scene.update()
-    
+
 # Runs the method
 if __name__ == "__main__":
     with open("molecule.json") as molecule_file:
--- molecule_to_json.py
+++ molecule_to_json.py
@@ -12,33 +12,35 @@ import json
 from json import encoder
 encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 
+
 def molecule_to_json(molecule):
     """ Converts an OpenBabel molecule to json for use in Blender """
-    
+
     # Get centroid to center molecule at (0, 0, 0)
     centroid = [0, 0, 0]
     for atom in molecule.atoms:
-        centroid = [c+a for c, a in zip(centroid, atom.coords)]
-    centroid = [c/float(len(molecule.atoms)) for c in centroid]
-    
+        centroid = [c + a for c, a in zip(centroid, atom.coords)]
+    centroid = [c / float(len(molecule.atoms)) for c in centroid]
+
     # Openbabel atom types have valence ints. Remove those.
     # There are other flags on common atoms (aromatic, .co, am, etc.)
     parse_type = lambda t: t[0] if len(t) > 2 else re.sub("(\d|\W)", "", t)
-    
+
     # Save atom element type and 3D location.
-    atoms = [{"element": parse_type(atom.type), 
-              "location": [a-c for a,c in zip(atom.coords, centroid)]} 
+    atoms = [{"element": parse_type(atom.type),
+              "location": [a - c for a, c in zip(atom.coords, centroid)]}
              for atom in molecule.atoms]
-    
+
     # Save number of bonds and indices of endpoint atoms
     # Switch from 1-index to 0-index counting
-    bonds = [{"source": b.GetBeginAtom().GetIndex(), 
+    bonds = [{"source": b.GetBeginAtom().GetIndex(),
               "target": b.GetEndAtom().GetIndex(),
-              "order": b.GetBondOrder()} for b in OBMolBondIter(molecule.OBMol)]
-              
-    json_string = json.dumps({"atoms": atoms, "bonds": bonds}, 
-                              sort_keys=True, indent=4)
-    
+              "order": b.GetBondOrder()}
+             for b in OBMolBondIter(molecule.OBMol)]
+
+    json_string = json.dumps({"atoms": atoms, "bonds": bonds},
+                             sort_keys=True, indent=4)
+
     # An obsessive-compulsive hack to display float lists as one line in json
     json_string = json_string.split('\n')
     for i, row in enumerate(json_string):
@@ -47,25 +49,26 @@ def molecule_to_json(molecule):
             continue
         # Move down rows until the list ends, deleting and appending.
         while _has_next_float(json_string, i):
-            row += " " + json_string[i+1].strip()
-            del json_string[i+1]
+            row += " " + json_string[i + 1].strip()
+            del json_string[i + 1]
         # Finish off with the closing bracket
-        json_string[i] = row + " " + json_string[i+1].strip()
-        del json_string[i+1]
+        json_string[i] = row + " " + json_string[i + 1].strip()
+        del json_string[i + 1]
     # Recombine the list into a string and return
     return "\n".join(json_string)
+
 
 def _has_next_float(json_string, i):
     """ Tests if the next row in a split json string is a float """
     try:
-        float(json_string[i+1].strip().replace(",",""))
+        float(json_string[i + 1].strip().replace(",", ""))
         return True
     except:
-        return False 
-    
+        return False
+
 if __name__ == "__main__":
     from sys import argv, exit
-    
+
     # Print help if needed
     if len(argv) < 3 or "--help" in argv:
         print "USAGE: python molecule_to_json.py *type* *data* (--addh)"
@@ -73,26 +76,26 @@ if __name__ == "__main__":
         print "    data: Chemical file or string"
         print "    --addh:  add hydrogen atoms"
         exit()
-    
+
     # "smi", "mol", "cif", etc.
     type = argv[1]
-    
+
     # Support both files and strings.
     try:
-       with open(argv[2]) as in_file:
-           in_data = in_file.read()
+        with open(argv[2]) as in_file:
+            in_data = in_file.read()
     except:
         in_data = argv[2]
-    
+
     # Load openbabel molecule
     molecule = pybel.readstring(type, in_data)
     molecule.addh()
-    
+
     # User specified args to generate coordinates and keep hydrogen atoms
     if type == "smi":
         molecule.make3D(steps=500)
     if "--addh" not in argv:
         molecule.removeh()
-    
+
     # Print result to stdout for piping and what not
     print molecule_to_json(molecule)
